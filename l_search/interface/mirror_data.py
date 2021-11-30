afc027a936ee03eb162d0412f825093e4e101fbb
simon <simoncoming321@163.com>
2021-11-30 17:17:46 +0800

    新增数据抽取 未测试

--- l_search/interface/mirror_data.py
+++ l_search/interface/mirror_data.py
@@ -7,7 +7,9 @@
 from flask_restx import Namespace, Resource, fields, marshal
 from l_search import models
 from l_search.models import db, search
+from l_search.models.base import get_source_db_meta
 from flask_msearch.backends import get_tables
+from DWMM.source_meta_operate.handle.meta_handle import MetaDetector
 
 api_mirror = Namespace('mirror_data', description='main function')
 
@@ -30,3 +32,14 @@ class ImportData(Resource):
         models.FullTest.create(**request_data)
         search.create_index(update=True)
         return {"title": "11", "content": "ddd"}, 200
+
+
+class MirrorData(Resource):
+
+    @api_mirror.doc("add all")
+    def get(self):
+        source_db = get_source_db_meta()
+        cc = source_db.classes
+        for t in source_db.classes:
+            search.create_index(t, update=True)
+
